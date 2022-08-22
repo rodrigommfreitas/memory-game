363a16d602fb7521e7de38fa817f3e09a0e0f9f0
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2022-08-22 03:30:08 +0100

    Start working on the main game component

---
+++ src/components/Game.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Game = () => {
+  const totalPokemons = 151;
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  return <div>Game</div>;
+};
+
+export default Game;
