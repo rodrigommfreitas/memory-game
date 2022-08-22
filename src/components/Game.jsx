ed01dc178d012f2e638aa4ea31771d2f2b4f2974
Rodrigo Freitas <rodrigommfreitas@protonmail.com>
2022-08-22 03:31:57 +0100

    Create addScore and resetScore functions

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,6 +5,15 @@ const Game = () => {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
 
+  const addScore = () => {
+    const newScore = score + 1;
+    if (newScore > bestScore) setBestScore(newScore);
+    setScore(newScore);
+  };
+  const resetScore = () => {
+    setScore(0);
+  };
+
   return <div>Game</div>;
 };
 
